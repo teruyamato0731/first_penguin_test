7cc2c1f47f2818012394e77e5ac7f0606b8c9149
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-07-16 23:50:40 +0900

    feat: コードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include <mbed.h>
 
+constexpr uint32_t can_id = 30;
+
 BufferedSerial pc{USBTX, USBRX, 115200};
 
 // CAN can{PA_11, PA_12, (int)1e6};
-// CAN can{PB_12, PB_13, (int)1e6};
-// CANMessage msg;
+CAN can{PB_12, PB_13, (int)1e6};
+CANMessage msg;
 
 Timer timer;
+
+struct FirstPenguin {
+  static constexpr int max = INT16_MAX;
+  uint32_t send_id;
+  int16_t pwm[4];
+  struct {
+    int32_t enc;
+    uint32_t adc;
+
+    void set(const uint8_t data[8]) {
+      memcpy(this, data, sizeof(*this));
+    }
+  } receive[4];
+
+  void read(const CANMessage& msg) {
+    if(msg.format == CANStandard && msg.type == CANData && msg.len == sizeof(receive[0])) {
+      receive[msg.id - send_id - 1].set(msg.data);
+    }
+  }
+  void send() {
+    can.write(CANMessage{send_id, reinterpret_cast<const uint8_t*>(pwm), 8});
+  }
+};
+
+FirstPenguin penguin{can_id};
 
 int main() {
   printf("\nsetup\n");
@@ -14,8 +41,16 @@ int main() {
   auto pre = timer.elapsed_time();
   while(1) {
     auto now = timer.elapsed_time();
+
+    if(can.read(msg)) {
+      penguin.read(msg);
+    }
+
     if(now - pre > 20ms) {
-      printf("hoge\n");
+      for(auto e: penguin.receive) printf("% 5d\t", e.enc);
+      printf("\n");
+      for(auto& e: penguin.pwm) e = penguin.max / 4;
+      penguin.send();
       pre = now;
     }
   }
