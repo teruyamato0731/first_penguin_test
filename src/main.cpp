2bd101d01fe05a019dc6033c4a84eda50769084e
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-07-21 19:04:07 +0900

    feat: adcの表示と軽微な修正

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,21 @@ Timer timer;
 struct FirstPenguin {
   static constexpr int max = INT16_MAX;
   uint32_t send_id;
-  int16_t pwm[4];
+  int16_t pwm[4] = {};
   struct {
     int32_t enc;
     uint32_t adc;
-
     void set(const uint8_t data[8]) {
       memcpy(this, data, sizeof(*this));
     }
-  } receive[4];
-
+  } receive[4] = {};
   void read(const CANMessage& msg) {
     if(msg.format == CANStandard && msg.type == CANData && msg.len == sizeof(receive[0])) {
       receive[msg.id - send_id - 1].set(msg.data);
     }
   }
-  void send() {
-    can.write(CANMessage{send_id, reinterpret_cast<const uint8_t*>(pwm), 8});
+  bool send() {
+    return can.write(CANMessage{send_id, reinterpret_cast<const uint8_t*>(pwm), 8});
   }
 };
 
@@ -47,7 +45,8 @@ int main() {
     }
 
     if(now - pre > 20ms) {
-      for(auto e: penguin.receive) printf("% 5d\t", e.enc);
+      for(auto e: penguin.receive) printf("% 5ld\t", e.enc);
+      for(auto e: penguin.receive) printf("% 5ld\t", e.adc);
       printf("\n");
       for(auto& e: penguin.pwm) e = penguin.max / 4;
       penguin.send();
